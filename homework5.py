c4f80b194f04ba038133f8248a74b5cd2b22713d
Darima98 <165848832+Darima98@users.noreply.github.com>
2024-04-11 18:52:14 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'kiwi', 'cherry', 'mango', 'melon']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[3] = 'strawsberry'
+print(my_list)
+my_dict = {'apple': 'яблоко', 'house': 'дом', 'window': 'окно'}
+print(my_dict)
+print(my_dict['house'])
+my_dict['house'] = 'дача'
+print(my_dict)
